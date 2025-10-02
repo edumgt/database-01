745e923d50322e4c527f6913507972bc08fe27c2
edumgt <kimdypm@gmail.com>
2025-10-02 14:27:47 +0900

    1

--- test.sql
+++ test.sql
@@ -1,16 +1,70 @@
 
-CREATE TABLE student
+CREATE TABLE Company
 (
-  id   varchar(10) NOT NULL,
-  name VARCHAR(50) NOT NULL,
+  company_id   VARCHAR(10) NOT NULL DEFAULT 1234567890 COMMENT '사업자번호_PK',
+  company_name VARCHAR(50) NOT NULL DEFAULT '' COMMENT '회사명',
+  PRIMARY KEY (company_id)
+) COMMENT '회사정보';
+
+CREATE TABLE customers
+(
+  user_id   VARCHAR(50)          NOT NULL COMMENT '구매자아이디',
+  user_name VARCHAR(50)          NOT NULL COMMENT '관리자이름',
+  user_type ENUM('SA','CW','CP') NOT NULL COMMENT '관리자종류',
+  password  VARCHAR(200)         NOT NULL COMMENT '비번',
+  PRIMARY KEY (user_id)
+) COMMENT '사용자';
+
+CREATE TABLE Employee
+(
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890 COMMENT '사업자번호_PK',
+  emp_id     VARCHAR(7)  NOT NULL DEFAULT 2020001 COMMENT '직원번호',
+  emp_name   VARCHAR(50) NOT NULL DEFAULT '' COMMENT '직원명',
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890 COMMENT '사업자번호_PK',
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890 COMMENT '사업자번호_PK',
+  PRIMARY KEY (emp_id)
+) COMMENT '직원정보';
+
+CREATE TABLE member
+(
+  id      varchar(10) NOT NULL COMMENT 'M_id',
+  name    VARCHAR(50) NOT NULL,
+  isAdult BOOLEAN     NOT NULL DEFAULT True COMMENT '성인이야?',
+  id      varchar(10) NOT NULL COMMENT 'P_id',
   PRIMARY KEY (id)
 ) COMMENT 'student';
+
+CREATE TABLE order_cancel
+(
+  user_id VARCHAR(50) NOT NULL COMMENT '구매자아이디',
+  p_id    INT         NOT NULL DEFAULT 10000 COMMENT '상품고유번호'
+) COMMENT '주문취소';
+
+CREATE TABLE ordering
+(
+  user_id VARCHAR(50) NOT NULL COMMENT '구매자아이디',
+  p_id    INT         NOT NULL DEFAULT 10000 COMMENT '상품고유번호'
+) COMMENT '주문';
+
+CREATE TABLE product
+(
+  p_id   INT          NOT NULL DEFAULT 10000 COMMENT '상품고유번호',
+  p_name VARCHAR(200) NOT NULL DEFAULT 이름 COMMENT '이름',
+  PRIMARY KEY (p_id)
+) COMMENT '상품';
+
+CREATE TABLE s_u
+(
+  s_u_i          NULL    ,
+  hakbun VARCHAR NULL     COMMENT 'IX'
+);
 
 CREATE TABLE student_subject
 (
   id     VARCHAR(10) NOT NULL DEFAULT rowId(),
   stu_id varchar(10) NOT NULL,
   sub_id VARCHAR(3)  NOT NULL,
+  id     varchar(10) NOT NULL,
   PRIMARY KEY (id)
 ) COMMENT '다대다의 수강신청';
 
@@ -21,13 +75,42 @@ CREATE TABLE subject
   PRIMARY KEY (id)
 ) COMMENT 'subject';
 
-
-ALTER TABLE student_subject
-  ADD CONSTRAINT FK_student_TO_student_subject
-    FOREIGN KEY (stu_id)
-    REFERENCES student (id);
-
 ALTER TABLE student_subject
   ADD CONSTRAINT FK_subject_TO_student_subject
     FOREIGN KEY (sub_id)
     REFERENCES subject (id);
+
+ALTER TABLE student_subject
+  ADD CONSTRAINT FK_member_TO_student_subject
+    FOREIGN KEY (id)
+    REFERENCES member (id);
+
+ALTER TABLE Employee
+  ADD CONSTRAINT FK_Company_TO_Employee
+    FOREIGN KEY (company_id)
+    REFERENCES Company (company_id);
+
+ALTER TABLE ordering
+  ADD CONSTRAINT FK_customers_TO_ordering
+    FOREIGN KEY (user_id)
+    REFERENCES customers (user_id);
+
+ALTER TABLE ordering
+  ADD CONSTRAINT FK_product_TO_ordering
+    FOREIGN KEY (p_id)
+    REFERENCES product (p_id);
+
+ALTER TABLE order_cancel
+  ADD CONSTRAINT FK_customers_TO_order_cancel
+    FOREIGN KEY (user_id)
+    REFERENCES customers (user_id);
+
+ALTER TABLE order_cancel
+  ADD CONSTRAINT FK_product_TO_order_cancel
+    FOREIGN KEY (p_id)
+    REFERENCES product (p_id);
+
+ALTER TABLE member
+  ADD CONSTRAINT FK_member_TO_member
+    FOREIGN KEY (id)
+    REFERENCES member (id);
--- work.sql
+++ work.sql
@@ -1,67 +1,254 @@
--- test.city definition
 
-CREATE TABLE `city` (
-  `city_code` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `city_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `city_ename` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `city_x` decimal(9,6) DEFAULT NULL,
-  `city_y` decimal(9,6) DEFAULT NULL,
-  `nation_code` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`city_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE Company
+(
+  company_id   VARCHAR(10) NOT NULL DEFAULT 1234567890,
+  company_name VARCHAR(50) NOT NULL DEFAULT '',
+  CONSTRAINT PK_Company PRIMARY KEY (company_id)
+)
+GO
 
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '회사정보', 'user', dbo, 'table', 'Company'
+GO
 
--- test.company definition
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '사업자번호_PK', 'user', dbo, 'table', 'Company', 'column', 'company_id'
+GO
 
-CREATE TABLE `company` (
-  `company_id` varchar(10) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1234567890' COMMENT '사업자번호_PK',
-  `company_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '회사명',
-  PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='회사정보';
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '회사명', 'user', dbo, 'table', 'Company', 'column', 'company_name'
+GO
 
+CREATE TABLE customers
+(
+  user_id   VARCHAR(50)          NOT NULL,
+  user_name VARCHAR(50)          NOT NULL,
+  user_type ENUM('SA','CW','CP') NOT NULL,
+  password  VARCHAR(200)         NOT NULL,
+  CONSTRAINT PK_customers PRIMARY KEY (user_id)
+)
+GO
 
--- test.nation definition
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '사용자', 'user', dbo, 'table', 'customers'
+GO
 
-CREATE TABLE `nation` (
-  `nation_code` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `nation_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `nation_sudo` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nation_pop` bigint DEFAULT NULL,
-  `nation_gdp` bigint DEFAULT NULL,
-  PRIMARY KEY (`nation_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '구매자아이디', 'user', dbo, 'table', 'customers', 'column', 'user_id'
+GO
 
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '관리자이름', 'user', dbo, 'table', 'customers', 'column', 'user_name'
+GO
 
--- test.student definition
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '관리자종류', 'user', dbo, 'table', 'customers', 'column', 'user_type'
+GO
 
-CREATE TABLE `student` (
-  `id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '학생 ID (PK)',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '이름',
-  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '주소',
-  `grade` int NOT NULL COMMENT '학년',
-  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '성별',
-  `class` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '반 코드',
-  `age` tinyint DEFAULT '1' COMMENT '나이',
-  `hobby` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='학생 정보';
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '비번', 'user', dbo, 'table', 'customers', 'column', 'password'
+GO
 
+CREATE TABLE Employee
+(
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890,
+  emp_id     VARCHAR(7)  NOT NULL DEFAULT 2020001,
+  emp_name   VARCHAR(50) NOT NULL DEFAULT '',
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890,
+  company_id VARCHAR(10) NOT NULL DEFAULT 1234567890,
+  CONSTRAINT PK_Employee PRIMARY KEY (emp_id)
+)
+GO
 
--- test.student_subject definition
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '직원정보', 'user', dbo, 'table', 'Employee'
+GO
 
-CREATE TABLE `student_subject` (
-  `stu_id` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `sub_id` varchar(3) COLLATE utf8mb4_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='수강신청';
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '사업자번호_PK', 'user', dbo, 'table', 'Employee', 'column', 'company_id'
+GO
 
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '직원번호', 'user', dbo, 'table', 'Employee', 'column', 'emp_id'
+GO
 
--- test.employee definition
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '직원명', 'user', dbo, 'table', 'Employee', 'column', 'emp_name'
+GO
 
-CREATE TABLE `employee` (
-  `company_id` varchar(10) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1234567890' COMMENT '사업자번호_PK',
-  `emp_id` varchar(7) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '2020001' COMMENT '직원번호',
-  `emp_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '직원명',
-  PRIMARY KEY (`emp_id`),
-  KEY `FK_Company_TO_Employee` (`company_id`),
-  CONSTRAINT `FK_Company_TO_Employee` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='직원정보';
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '사업자번호_PK', 'user', dbo, 'table', 'Employee', 'column', 'company_id'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '사업자번호_PK', 'user', dbo, 'table', 'Employee', 'column', 'company_id'
+GO
+
+CREATE TABLE member
+(
+  id      varchar(10) NOT NULL,
+  name    VARCHAR(50) NOT NULL,
+  isAdult BOOLEAN     NOT NULL DEFAULT True,
+  id      varchar(10) NOT NULL,
+  CONSTRAINT PK_member PRIMARY KEY (id)
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  'student', 'user', dbo, 'table', 'member'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  'M_id', 'user', dbo, 'table', 'member', 'column', 'id'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '성인이야?', 'user', dbo, 'table', 'member', 'column', 'isAdult'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  'P_id', 'user', dbo, 'table', 'member', 'column', 'id'
+GO
+
+CREATE TABLE order_cancel
+(
+  user_id VARCHAR(50) NOT NULL,
+  p_id    INT         NOT NULL DEFAULT 10000
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '주문취소', 'user', dbo, 'table', 'order_cancel'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '구매자아이디', 'user', dbo, 'table', 'order_cancel', 'column', 'user_id'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '상품고유번호', 'user', dbo, 'table', 'order_cancel', 'column', 'p_id'
+GO
+
+CREATE TABLE ordering
+(
+  user_id VARCHAR(50) NOT NULL,
+  p_id    INT         NOT NULL DEFAULT 10000
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '주문', 'user', dbo, 'table', 'ordering'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '구매자아이디', 'user', dbo, 'table', 'ordering', 'column', 'user_id'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '상품고유번호', 'user', dbo, 'table', 'ordering', 'column', 'p_id'
+GO
+
+CREATE TABLE product
+(
+  p_id   INT          NOT NULL DEFAULT 10000,
+  p_name VARCHAR(200) NOT NULL DEFAULT 이름,
+  CONSTRAINT PK_product PRIMARY KEY (p_id)
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '상품', 'user', dbo, 'table', 'product'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '상품고유번호', 'user', dbo, 'table', 'product', 'column', 'p_id'
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '이름', 'user', dbo, 'table', 'product', 'column', 'p_name'
+GO
+
+CREATE TABLE s_u
+(
+  s_u_i         ,
+  hakbun VARCHAR
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  'IX', 'user', dbo, 'table', 's_u', 'column', 'hakbun'
+GO
+
+CREATE TABLE student_subject
+(
+  id     VARCHAR(10) NOT NULL DEFAULT rowId(),
+  stu_id varchar(10) NOT NULL,
+  sub_id VARCHAR(3)  NOT NULL,
+  id     varchar(10) NOT NULL,
+  CONSTRAINT PK_student_subject PRIMARY KEY (id)
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  '다대다의 수강신청', 'user', dbo, 'table', 'student_subject'
+GO
+
+CREATE TABLE subject
+(
+  id   VARCHAR(3)  NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  CONSTRAINT PK_subject PRIMARY KEY (id)
+)
+GO
+
+EXECUTE sys.sp_addextendedproperty 'MS_Description',
+  'subject', 'user', dbo, 'table', 'subject'
+GO
+
+ALTER TABLE student_subject
+  ADD CONSTRAINT FK_subject_TO_student_subject
+    FOREIGN KEY (sub_id)
+    REFERENCES subject (id)
+GO
+
+ALTER TABLE student_subject
+  ADD CONSTRAINT FK_member_TO_student_subject
+    FOREIGN KEY (id)
+    REFERENCES member (id)
+GO
+
+ALTER TABLE Employee
+  ADD CONSTRAINT FK_Company_TO_Employee
+    FOREIGN KEY (company_id)
+    REFERENCES Company (company_id)
+GO
+
+ALTER TABLE ordering
+  ADD CONSTRAINT FK_customers_TO_ordering
+    FOREIGN KEY (user_id)
+    REFERENCES customers (user_id)
+GO
+
+ALTER TABLE ordering
+  ADD CONSTRAINT FK_product_TO_ordering
+    FOREIGN KEY (p_id)
+    REFERENCES product (p_id)
+GO
+
+ALTER TABLE order_cancel
+  ADD CONSTRAINT FK_customers_TO_order_cancel
+    FOREIGN KEY (user_id)
+    REFERENCES customers (user_id)
+GO
+
+ALTER TABLE order_cancel
+  ADD CONSTRAINT FK_product_TO_order_cancel
+    FOREIGN KEY (p_id)
+    REFERENCES product (p_id)
+GO
+
+ALTER TABLE member
+  ADD CONSTRAINT FK_member_TO_member
+    FOREIGN KEY (id)
+    REFERENCES member (id)
+GO
